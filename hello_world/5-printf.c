1b45e74404c22ab59508294ca769bc090f9796f2
karenmendez7 <karenmendez7@hotmail.com>
2022-10-10 23:16:55 -0500

    tarea 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>i
 
 /**
 * main - prints chars
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,")
+printf("with proper grammar, but the outcome is a piece of art,");
 
 return (0);
 }
