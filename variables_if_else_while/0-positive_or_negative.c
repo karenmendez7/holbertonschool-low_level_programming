5f66e11619d3c60affc462f4d4b66cc650b47c0a
karenmendez7 <karenmendez7@hotmail.com>
2022-10-11 23:55:22 -0500

    tarea 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+* main - entry point
+* Return: 0
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
