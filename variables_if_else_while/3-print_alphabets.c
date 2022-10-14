056046dedb8afd350a57abfe5218cd8cfebb5550
karenmendez7 <karenmendez7@hotmail.com>
2022-10-13 20:45:11 -0500

    tarea 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase, and then in uppercase
+* Return: 0
+*/
+
+int main(void)
+
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+for (i = 'A'; i < 'Z'; i++)
+putchar (i);
+putchar (i);
+
+return (0);
+
+}
