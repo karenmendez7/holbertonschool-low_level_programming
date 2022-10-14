cccf8a20eaa2afe9628cdfb921f6760937d13472
karenmendez7 <karenmendez7@hotmail.com>
2022-10-13 19:18:17 -0500

    tarea 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ printf("Last digit of %d and is greater than 5\n", n);
 if ((n % 10) == 0)
 printf("Last digit of %d and is 0\n", n);
 
-if ((n % 10) < 6 && != 0)
+if ((n % 10) < 6 && (n % 10 != 0)
 printf("Last digit of %d and is less than 6 and not 0\n", n);
 
 return (0);
