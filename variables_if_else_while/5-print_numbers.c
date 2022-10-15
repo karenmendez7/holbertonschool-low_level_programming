bcf5a38b1e6dc59c43062cf1c9462e923723a4b1
karenmendez7 <karenmendez7@hotmail.com>
2022-10-14 21:30:25 -0500

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Prints all digits numbers base 10
+* Return: 0
+*/
+
+int main(void)
+{
+char i;
+
+for (i = 0; i < 10; i++)
+
+putchar(i);
+
+putchar('\n');
+
+return (0);
+
+}
+
