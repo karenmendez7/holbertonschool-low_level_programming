871ff27ea2fa82adfb28179f90b166fe0f74ed1e
karenmendez7 <karenmendez7@hotmail.com>
2022-10-15 18:24:33 -0500

    tarea 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - prints
+* Return: 0
+*/
+
+int main(void)
+{
+char i[8] = "_putchar";
+int a;
+
+for (a = 0; a <= 8; a++)
+
+_putchar(i[a]);
+
+_putchar('\n');
+return (0);
+}
