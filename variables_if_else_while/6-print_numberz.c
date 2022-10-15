1e5c43b4720755264057765f0a03002e9480ad93
karenmendez7 <karenmendez7@hotmail.com>
2022-10-14 21:39:12 -0500

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Prints all digits numbers base 10
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+
+putchar(i);
+
+putchar('\n');
+
+return (0);
+
+}
