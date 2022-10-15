5bf0aaa76deea9b4720e7a19a72596affa6a21aa
karenmendez7 <karenmendez7@hotmail.com>
2022-10-14 21:09:14 -0500

    4-print_alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - print alphabet
+* Return: 0
+*/
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i < 'z'; i++)
+{
+if (i != 'q' && i != 'e')
+
+
+putchar(i);
+}
+
+putchar('\n');
+
+return (0);
+
+}
+
