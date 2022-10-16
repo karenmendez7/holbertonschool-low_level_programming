8563515dbadceeec2eb51143b023feac753fd182
karenmendez7 <karenmendez7@hotmail.com>
2022-10-15 19:05:47 -0500

    tarea 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet x10
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+char a;
+char i;
+
+for (a = "a"; a <= 'z'; a++)
+for (a = '0'; a <= '10'; a++)
+
+_putchar(a);
+_putchar('\n');
+
+}
