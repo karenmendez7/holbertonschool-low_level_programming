8323bd1f3400d49c1ddd90a247a79376066f2645
karenmendez7 <karenmendez7@hotmail.com>
2022-10-15 19:20:13 -0500

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,13 @@ void print_alphabet_x10(void)
 {
 char a;
 int i = 0;
-while (i <= 10)
+while (i < 10)
 {
 for (a = 'a'; a <= 'z'; a++)
 
 _putchar(a);
-_putchar('\n');
 
 }
+_putchar('\n');
+i++
 }
