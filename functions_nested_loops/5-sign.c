be2e570e562a2064dae90e82148ff6ab244610fb
karenmendez7 <karenmendez7@hotmail.com>
2022-10-17 15:56:42 -0500

    tarea 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("%d +\n", n);
+printf('+');
 }
 return (1);
 if (n == 0)
 {
-printf("%d 0\n", n);
+printf('0');
 return (0);
 }
 if (n < 0)
 {
-printf("%c -\n", n);
+printf('-');
 return (-1);
 }
 
