a2c77343d4f6d48168ff13f13931a9cd3f8e0eb0
karenmendez7 <karenmendez7@hotmail.com>
2022-10-17 16:18:01 -0500

    tarea 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_sign - print sign
  * @n: variable
@@ -9,20 +8,22 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+_putchar('+');
 return (1);
 }
+{
 if (n == 0)
 {
-putchar('0');
+_putchar('0');
 return (0);
 }
 if (n < 0)
 {
-putchar('-');
+_putchar('-');
 return (-1);
 }
 
 return (0);
+}
 
 }
