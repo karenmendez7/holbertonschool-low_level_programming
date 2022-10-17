b9aa005e3a747ef7169e9b098e34027703ed0f55
karenmendez7 <karenmendez7@hotmail.com>
2022-10-17 13:31:05 -0500

    tarea 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: letter
+ *  Return (0)
+ */
+int _isalpha(int c)
+
+if (c > 65) && ( c <= 90)
+if (c > 97) && (c <= 122)
+{
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
