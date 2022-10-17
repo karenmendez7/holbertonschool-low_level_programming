18365c9c9e0d8a6e353576dbe30826dcdbc520cf
karenmendez7 <karenmendez7@hotmail.com>
2022-10-17 14:03:54 -0500

    tarea 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - print sign
+ * @n: variable
+ * Return: 0
+*/
+int print_sign(int n)
+{
+if (n > 0 )
+{
+printf("%c +\n", n);
+
+return (1);
+}
+
+if( n == 0)
+{
+printf("%d 0\n", n);
+
+return (0);
+}
+if( n < 0)
+{
+printf("%c -\n", n);
+
+return (-1);
+}
+
+}
