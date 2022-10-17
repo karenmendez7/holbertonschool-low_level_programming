7ea45feb005906e4f425507746ebd5e74e9a0c11
karenmendez7 <karenmendez7@hotmail.com>
2022-10-17 12:37:10 -0500

    tarea 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int _islower - print and check lowercase
+ * Return: 0
+ */
+int _islower(int c)
+{
+if (C > 97 && C <= 122)
+{
+    return (1);
+}
+else
+{
+    return (0)
+}
+}
