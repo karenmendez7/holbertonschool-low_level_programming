caedff0b80385612846a55580a5cb2c187949933
karenmendez7 <karenmendez7@hotmail.com>
2022-10-17 19:14:00 -0500

    tarea 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,14 @@
 # include "main.h"
 /**
  * print_to_98 - print to 98
- * 
+ * Return: 0
+ * @n: number
  */
+void print_to_98(int n)
+{
+for (; n <= 98; n++)
+putchar ('n \n');
+for (; n > 98; n--)
+putchar ('n \n');
+
+}
