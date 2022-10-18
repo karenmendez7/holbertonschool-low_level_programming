384211e564e6c1bb531653e7f07ad6bdbb0be1c7
karenmendez7 <karenmendez7@hotmail.com>
2022-10-17 19:58:01 -0500

    tarea 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,13 @@ void print_to_98(int n)
 {
 for (; n < 98; n++)
 {
-printf("%d, \n", n);
+printf("%d,  ", n);
 }
 for (; n > 98; n--)
 {
-printf("%d, \n", n);
+printf("%d,  ", n);
 }
 if (n == 98)
-printf("%d \n", n);
+printf("%d  ", n);
 
 }
