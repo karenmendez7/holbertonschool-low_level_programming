d16cb7d536bcd625c29254a004bba580d55a98b3
karenmendez7 <karenmendez7@hotmail.com>
2022-10-18 23:55:35 -0500

    tarea 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - return length of a string
+ * Return: 0
+ * @s: 0
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; i < NULL; i++)
+{
+if (*s != NULL)
+return (i);
+}
+
+}
