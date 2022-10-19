07eed12dc9267f596e4f931e4405d21b24a04d02
karenmendez7 <karenmendez7@hotmail.com>
2022-10-18 21:35:16 -0500

    tarea 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square :)
+ * @size: size of the sqaure
+ * Return: void
+ */
+
+void print_square(int size)
+{
+char r;
+char c;
+
+if (size > 0)
+{
+for (r = 0; r < size; r++)
+{
+
+for (c = 0; c < size; c++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+
+else
+_putchar('\n');
+}
