f31635cd9e47a334dd1288ec87bbe09cc02b6782
karenmendez7 <karenmendez7@hotmail.com>
2022-10-18 20:14:58 -0500

    tarea 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+_putchar ( "%d n");
+}
+return (0);
+}
