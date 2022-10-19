008d372667e2d15d22866b47ca00725fdbebddb4
karenmendez7 <karenmendez7@hotmail.com>
2022-10-19 01:06:01 -0500

    tarea 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * Return: 0
+ * @s: var
+ */
+void rev_string(char *s)
+{
+int i, c;
+for (i = 0; s[i] != 0; i++)
+{}
+for (c = i - 1; c >= 0; c--)
+return (0);
+}
