905e6c8f33f8c7330557552c37d204754cf7644b
karenmendez7 <karenmendez7@hotmail.com>
2022-10-20 20:36:04 -0500

    tarea 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - print every other char string
+ * Return: 0
+ * @str: string
+ */
+puts2(char *str)
+{
+char s;
+for (s = 0; *(str[s]; s++))
+if (str % 2 == 0)
+_puts(str[s]);
+
+return (0);
+
+}
