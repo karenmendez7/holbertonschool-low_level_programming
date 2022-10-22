200961e558437a6550c3b229884d085e98bdbe18
karenmendez7 <karenmendez7@hotmail.com>
2022-10-21 20:48:59 -0500

    tarea 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcpy - copies string pointed
+ * @dest: pointer dest
+ * @src: pointer src
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; i >= 0; i++)
+{
+src = dest;
+}
+return (dest);
+}
