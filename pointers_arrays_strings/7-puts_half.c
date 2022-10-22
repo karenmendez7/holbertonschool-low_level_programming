a208aab8873d0c2b2a6ef6429549717a0466b1d6
karenmendez7 <karenmendez7@hotmail.com>
2022-10-21 19:12:07 -0500

    tarea 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,10 +9,10 @@ void puts_half(char *str)
 int a, i;
 for (a = 0; *(str + a) != '\0'; a++)
 {
-if (a %2 == 1)
-i = a /2;
+if (a % 2 == 1)
+i = a / 2;
 else
-i = ( a - 1) /2;
+i = (a - 1) / 2;
 }
 for (i++; i < a; i++)
 {
