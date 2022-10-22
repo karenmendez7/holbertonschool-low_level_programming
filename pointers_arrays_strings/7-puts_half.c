7275402afc937042ab3d786797e6d31ab5cdb392
karenmendez7 <karenmendez7@hotmail.com>
2022-10-21 19:22:58 -0500

    tarea 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
- * puts_half - prints half of a string
- * Retunr: 0
- * @str: var
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: no return.
  */
 void puts_half(char *str)
 {
-int a, i;
+int a = 0, i;
+
 while (a >= 0)
 {
 if (str[a] == '\0')
 break;
 a++;
 }
-{
+
 if (a % 2 == 1)
 i = a / 2;
 else
 i = (a - 1) / 2;
-}
+
 for (i++; i < a; i++)
-{
-_putchar (str[i]);
-}
-_putchar ('\n');
+_putchar(str[i]);
+_putchar('\n');
 }
