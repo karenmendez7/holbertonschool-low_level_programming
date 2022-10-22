7b63eca72dcd84db1689aaa846e9631377833f82
karenmendez7 <karenmendez7@hotmail.com>
2022-10-21 19:09:05 -0500

    tarea 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * Retunr: 0
+ * @str: var
+ */
+void puts_half(char *str)
+{
+int a, i;
+for (a = 0; *(str + a) != '\0'; a++)
+{
+if (a %2 == 1)
+i = a /2;
+else
+i = ( a - 1) /2;
+}
+for (i++; i < a; i++)
+{
+_putchar (str[i]);
+_putchar ('\n');
+}
+}
