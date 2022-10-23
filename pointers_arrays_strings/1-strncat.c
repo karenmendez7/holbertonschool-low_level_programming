e30121f37e9e896c74ee2f2db0e7f7fedf4de521
karenmendez7 <karenmendez7@hotmail.com>
2022-10-22 23:14:23 -0500

    t1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i;
 int s;
-int n = 0;
 
 for (i = 0; dest[i]; i++)
 
 ;
-if (n >= 0)
-for (s = 0; src[s]; s++)
+
+for (s = 0; s < n && src[s]; s++)
 
 {
 dest[i] = src[s];
