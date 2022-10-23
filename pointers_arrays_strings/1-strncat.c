925f3d9e2c4151c3e6932d140d1b3836f7818a70
karenmendez7 <karenmendez7@hotmail.com>
2022-10-22 22:35:49 -0500

    t1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: pointer dest
+ * @src: pointer src
+ * @n: var n
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int n;
+
+for (i = 0; dest[i]; i++)
+
+;
+
+for (n >= 0; src[n]; s++)
+{
+dest[i] = src[n];
+i++;
+}
+
+return (dest);
+
+}
