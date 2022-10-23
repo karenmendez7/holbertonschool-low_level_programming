5c077f952fbea822da0b6032a067f32795bbdf24
karenmendez7 <karenmendez7@hotmail.com>
2022-10-22 21:13:11 -0500

    tarea 1_

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest:dest string
+ * @src:src
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, s;
+for (i = 0; i = dest[i]; i++)
+
+;
+
+for (i = 0; i = src[s]; i++)
+{
+dest[i] = src[s];
+i++;
+}
+
+return (dest);
+
+}
