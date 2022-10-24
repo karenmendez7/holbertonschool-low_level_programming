a2b01bbc812ec181fc247b97c0e25eee546dfd02
karenmendez7 <karenmendez7@hotmail.com>
2022-10-24 00:25:59 -0500

    t3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer s1
+ * @s2: pointer s2
+ * Return: a
+ */
+int _strcmp(char *s1, char *s2)
+{
+int s = 0;
+int a = 0;
+
+while (s1[s] == s2[s])
+{
+if (s1[s] == '\0')
+return (0);
+s++;
+}
+a = (s1[i] - s2[i]);
+return (a);
+}
