65bc6104e7d9422c02abcb70ed8ea54c7ffe901e
karenmendez7 <karenmendez7@hotmail.com>
2022-10-23 23:10:09 -0500

    tarea 3

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: pointer dest
+ * @src: pointer src
+ * @n: var n
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+int n;
+for (i = 0; dest[n]; i++)
+;
+for (i = 0; src[n]; i++)
+{
+dest[n] = src[n]
+}
+}
