767c33254d09ffc1f33b96dfd66c016092397427
karenmendez7 <karenmendez7@hotmail.com>
2022-10-23 23:36:10 -0500

    t2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,11 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; dest[n]; i++)
-;
-for (i = 0; src[n]; i++)
-{
-dest[n] = src[n];
-}
-return (0);
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+
+return dest;
 }
